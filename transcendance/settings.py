30a861f7f1c74ce74683e79b9df6a7d62ffc23a8
demenciel <alexcouture5@hotmail.com>
2024-02-08 19:34:47 -0500

    Added installated apps in settings and create the models.py

--- transcendance/settings.py
+++ transcendance/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'transcendance',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'channels',
 ]
 
 MIDDLEWARE = [
