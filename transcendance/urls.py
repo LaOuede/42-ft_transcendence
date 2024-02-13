31c6b87a89460db76165f8dd1f22f2f53e2d5233
Alexandre Couture <acouture@c1r8p10.42quebec.com>
2024-02-13 15:18:49 -0500

    Added back the admin url

--- transcendance/urls.py
+++ transcendance/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('play/', views.play, name='play'),
     path('tournaments/', views.tournaments, name='tournaments'),
+    path('admin/', admin.site.urls),
 ]
 
