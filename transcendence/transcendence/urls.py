20aa5b2720b6d3558c4bdad6deccac6e5c2d0cc4
Alexandre Couture <acouture@c1r8p9.42quebec.com>
2024-03-04 14:31:21 -0500

    url

--- transcendence/transcendence/urls.py
+++ transcendence/transcendence/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 	2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
 from custom_auth import views as custom_auth_views
 from user import views
