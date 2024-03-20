1382f7dd234a0ef5474478abf9ef0a9c91018c91
demenciel <alexcouture5@hotmail.com>
2024-03-20 14:29:43 -0400

    fix favicon

--- transcendence/transcendence/urls.py
+++ transcendence/transcendence/urls.py
@@ -3,9 +3,14 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
 from base.views import index
+from django.http import HttpResponse
+
+def favicon(request):
+    return HttpResponse(status=204)
 
 urlpatterns = [
 	path("", index, name="index"),
+ 	path('favicon.ico', favicon),
 	path("pong/", include("pong.urls")),
 	path("user/", include("user.urls")),
 	path("game/", include("games_history.urls")),
