1d31831722b1d325caecb93523aba22827dbe421
Karim Mehour <kmehour@c1r8p11.42quebec.com>
2024-03-21 16:57:57 -0400

    Add friend list to user profile

--- transcendence/base/static/js/friends.js
+++ transcendence/base/static/js/friends.js
@@ -2,18 +2,18 @@ import {userStatusWebSocket} from "./websocket.js"
 
 let socket = new userStatusWebSocket()
 
-const loginBtn = document.getElementById("login")
-const logoutBtn = document.getElementById("logout")
-document.querySelector(".logbutton")
+// const loginBtn = document.getElementById("login")
+// const logoutBtn = document.getElementById("logout")
+// document.querySelector(".logbutton")
 
-logoutBtn.onclick = (e) => {
-    let user = "Dave";
-    let status = "OF";
-    socket.change_user_status(user, status);
-}
+// logoutBtn.onclick = (e) => {
+//     let user = "Dave";
+//     let status = "OF";
+//     socket.change_user_status(user, status);
+// }
 
-loginBtn.onclick = (e) => {
-    let user = "Dave";
-    let status = "ON";
-    socket.change_user_status(user, status);
-}
+// loginBtn.onclick = (e) => {
+//     let user = "Dave";
+//     let status = "ON";
+//     socket.change_user_status(user, status);
+// }
--- transcendence/user/views.py
+++ transcendence/user/views.py
@@ -88,6 +88,7 @@ def UserProfile(request):
 		serializer = UserSerializer(user)
 		user_data = serializer.data
 		user_data['activity'] = activity_display
+		user_data['friends'] = User.objects.all()
 		return render(request, 'profile.html', {'user_data': user_data})
 	return render(request, "base.html", {"content": "login.html"})
 
